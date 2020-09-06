f16738571316061898b24247d087a8e09317f358
unknown <netofwhat@gmail.com>
2020-09-06 08:27:12 +0500

    добавила тесты. запускаю в дебаге и смотрю

---
+++ Tests/BookExtentionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Extensions;
+using NUnit.Framework;
+using ORM.Contracts;
+
+namespace ORM.Tests
+{
+    [TestFixture]
+    public class BookExtentionsTests
+    {
+        [Test]
+        public void BookToStringTest()
+        {
+            var book = new Book
+            {
+                Author = "Auth",
+                Title = "T",
+                Id = "1",
+                Price = 1,
+                Skill = "S",
+                Weight = decimal.One
+            };
+            var bookStr = book.BookToString();
+        }
+    }
+}
---
+++ Tests/DataContextTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using ORM.Contracts;
+using ORM.Db;
+using FluentAssertions;
+
+namespace ORM.Tests
+{
+    [TestFixture]
+    public class DataContextTests
+    {
+        [Test]
+        public void FindWhenOkTest()
+        {
+            var dbEngine = new DbEngine();
+            dbEngine.Execute("add Id=000243DE,Title=The Ransom of Zarek,Price=35,Weight=1,Author=Marobar Sul,Skill=Athletics;");
+            dbEngine.Execute("add Id=000243EC,Title=The Warp in the West,Price=25,Weight=1,Author=Ulvius Tero,Skill=Block;");
+
+            var dataContext = new DataContext(dbEngine);
+            var book = dataContext.Find("000243EC");
+            book.Title.Should().Be("The Warp in the West");
+        }
+    }
+}
